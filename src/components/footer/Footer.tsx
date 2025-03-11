72d82bfcb133ad5a7e6fc06579df76f0af32376e
Edgar Perez 2 <edgarjoel9912@gmail.com>
2025-03-10 21:38:53 -0400

    update: Coloque mockup de metricas en la home, y actualice el footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,28 @@ const Footer: React.FC = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-gray-800 text-white text-center p-4 mt-6">
-            <p>Tejada Tech Group EIRL © {currentYear}. All Rights Reserved. Beta version 0.0.1</p>
+        // Footer
+        <footer className="bg-white">
+            <div className="mx-auto overflow-hidden px-6 py-2 sm:py-4 lg:px-8">
+                {/* <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
+                    {navigation.main.map((item) => (
+                        <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-900">
+                            {item.name}
+                        </a>
+                    ))}
+                </nav> */}
+                {/* <div className="mt-16 flex justify-center gap-x-10">
+                    {navigation.social.map((item) => (
+                        <a key={item.name} href={item.href} className="text-gray-600 hover:text-gray-800">
+                            <span className="sr-only">{item.name}</span>
+                            <item.icon aria-hidden="true" className="size-6" />
+                        </a>
+                    ))}
+                </div> */}
+                <p className="mt-10 text-center text-sm/6 text-gray-600"> &copy; {currentYear} Tejada Tech Group EIRL. All rights reserved. Beta version 0.0.1</p>
+            </div>
         </footer>
+
     );
 };
 
--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,3 +1,16 @@
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/20/solid'
+import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon } from '@heroicons/react/24/outline'
+
+const stats = [
+  { id: 1, name: 'Total Subscribers', stat: '71,897', icon: UsersIcon, change: '122', changeType: 'increase' },
+  { id: 2, name: 'Avg. Open Rate', stat: '58.16%', icon: EnvelopeOpenIcon, change: '5.4%', changeType: 'increase' },
+  { id: 3, name: 'Avg. Click Rate', stat: '24.57%', icon: CursorArrowRaysIcon, change: '3.2%', changeType: 'decrease' },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export default function Home() {
 
   function getFormattedDate(date: Date = new Date()): string {
@@ -14,42 +27,93 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-white p-6 md:p-8 lg:p-10 space-y-6">
-      {/* Contenedor principal*/}
-      <div className="bg-white p-6 md:p-8 lg:p-10 rounded-lg shadow-md mx-auto w-full max-w-5xl">
-        {/* Encabezado */}
-        <div className="bg-blue-500 text-white p-4 rounded-t-lg text-center sm:text-left">
-          <h2 className="text-lg font-bold">Configuración Principal</h2>
-        </div>
+    <>
 
-        {/* Contenido */}
-        <div className="border-t border-blue-500 mt-4 p-4 space-y-6">
-          {/* Vigencia Certificado */}
-          <div className="flex flex-col sm:flex-row sm:items-center gap-4">
-            <span className="font-semibold whitespace-nowrap">Vigencia Certificado Digital Hasta:</span>
-            <span>12 de marzo de 2025</span>
-            <a href="#" className="text-blue-500">Actualizar</a>
+
+      <div className="bg-white p-6 md:p-8 lg:p-10 space-y-6">
+        {/* Contenedor principal*/}
+        <div className="bg-white p-6 md:p-8 lg:p-10 rounded-lg shadow-md mx-auto w-full max-w-5xl">
+          {/* Encabezado */}
+          <div className="bg-blue-500 text-white p-4 rounded-t-lg text-center sm:text-left">
+            <h2 className="text-lg font-bold">Configuración Principal</h2>
           </div>
 
-          {/* Correo Electrónico */}
-          <div className="flex flex-col sm:flex-row sm:items-center gap-4">
-            <span className="font-semibold whitespace-nowrap truncate overflow-hidden">Correo para emitir documentos electrónicos:</span>
-            <span className="truncate overflow-hidden">no-reply@do.tejadatechgroup.com</span>
-            <div className="flex gap-x-4">
+          {/* Contenido */}
+          <div className="border-t border-blue-500 mt-4 p-4 space-y-6">
+            {/* Vigencia Certificado */}
+            <div className="flex flex-col sm:flex-row sm:items-center gap-4">
+              <span className="font-semibold whitespace-nowrap">Vigencia Certificado Digital Hasta:</span>
+              <span>12 de marzo de 2025</span>
               <a href="#" className="text-blue-500">Actualizar</a>
-              <a href="#" className="text-blue-500">
-                <i className="fas fa-question-circle"></i>
-              </a>
+            </div>
+
+            {/* Correo Electrónico */}
+            <div className="flex flex-col sm:flex-row sm:items-center gap-4">
+              <span className="font-semibold whitespace-nowrap truncate overflow-hidden">Correo para emitir documentos electrónicos:</span>
+              <span className="truncate overflow-hidden">no-reply@do.tejadatechgroup.com</span>
+              <div className="flex gap-x-4">
+                <a href="#" className="text-blue-500">Actualizar</a>
+                <a href="#" className="text-blue-500">
+                  <i className="fas fa-question-circle"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
+
+        {/* MÉTRICAS*/}
+        <div className="mt-6 text-center sm:text-left w-full max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
+
+          {/* <div className="mt-4 h-72 bg-gray-200 rounded-md"> */}
+
+          <div>
+            <h3 className="text-base font-semibold text-gray-900">Métricas de hoy, {getFormattedDate()} </h3>
+
+            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+              {stats.map((item) => (
+                <div
+                  key={item.id}
+                  className="relative overflow-hidden rounded-lg bg-white px-4 pt-5 pb-12 shadow-sm sm:px-6 sm:pt-6"
+                >
+                  <dt>
+                    <div className="absolute rounded-md bg-indigo-500 p-3">
+                      <item.icon aria-hidden="true" className="size-6 text-white" />
+                    </div>
+                    <p className="ml-16 truncate text-sm font-medium text-gray-500">{item.name}</p>
+                  </dt>
+                  <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+                    <p className="text-2xl font-semibold text-gray-900">{item.stat}</p>
+                    <p
+                      className={classNames(
+                        item.changeType === 'increase' ? 'text-green-600' : 'text-red-600',
+                        'ml-2 flex items-baseline text-sm font-semibold',
+                      )}
+                    >
+                      {item.changeType === 'increase' ? (
+                        <ArrowUpIcon aria-hidden="true" className="size-5 shrink-0 self-center text-green-500" />
+                      ) : (
+                        <ArrowDownIcon aria-hidden="true" className="size-5 shrink-0 self-center text-red-500" />
+                      )}
+
+                      <span className="sr-only"> {item.changeType === 'increase' ? 'Increased' : 'Decreased'} by </span>
+                      {item.change}
+                    </p>
+                    <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">
+                      <div className="text-sm">
+                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                          View all<span className="sr-only"> {item.name} stats</span>
+                        </a>
+                      </div>
+                    </div>
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+          {/* </div> */}
+        </div>
       </div>
 
-      {/* MÉTRICAS*/}
-      <div className="mt-6 text-center sm:text-left w-full max-w-5xl mx-auto px-6 md:px-8 lg:px-10">
-        <h3 className="font-semibold text-xl">Métricas de hoy, {getFormattedDate()} </h3>
-        <div className="mt-4 h-72 bg-gray-200 rounded-md"></div>
-      </div>
-    </div>
+    </>
   );
 }
