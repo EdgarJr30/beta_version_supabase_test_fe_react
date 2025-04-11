9e40118c40c5641a730e7511961c6108033a715c
Edgar Perez <edgarjoel9912@gmail.com>
2025-04-10 23:51:47 -0400

    Feat: Elimine el acceso de la ruta Admrol al rol admin

--- src/routes/rolePermissions.ts
+++ src/routes/rolePermissions.ts
@@ -1,6 +1,6 @@
 export const rolePermissions: Record<string, string[]> = {
     super_admin: ["/admin", "/tenant", "/testing", "/emision-eNCF", "/AnularSecuenciaAutorizada", "/aprobacion-eNCF", "/AdmCertificadoDigital", "/AdmRol", "/AdmUsuarios", "/"], // Super Admin puede acceder a todo
-    admin: ["/admin", "/tenant", "/testing", "/emision-eNCF", "/AnularSecuenciaAutorizada", "/aprobacion-eNCF", "/AdmCertificadoDigital", "/AdmRol", "/AdmUsuarios", "/"], // Admin puede acceder a todo
+    admin: ["/admin", "/tenant", "/testing", "/emision-eNCF", "/AnularSecuenciaAutorizada", "/aprobacion-eNCF", "/AdmCertificadoDigital", "/AdmUsuarios", "/"], // Admin puede acceder a todo
     user: ["/AdmUsuarios", "/emision-eNCF", "/AnularSecuenciaAutorizada", "/aprobacion-eNCF", "/AdmCertificadoDigital",], // Usuario normal solo puede acceder a /user
     testing: ["/testing"], // Testing solo puede acceder a /testing
   };
